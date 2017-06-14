9a90606249dc32aa0ee659704a3679c5133cce91
yangyunxiang161019 <yangyunxiang@rongcapital.cn>
2017-06-14 18:23:29 +0800

    add mybatisboot

---
+++ mybatisboot/src/main/java/com/exm/mybatisboot/entity/Loan.java
@@ -0,0 +1,79 @@
+package com.exm.mybatisboot.entity;
+
+//import com.exm.mybatisboot.common.entity.BaseEntity;
+
+public class Loan{
+
+    private int loanId;
+    private String loanNum;
+    private String custNum;
+    private String custName;
+    private String loanDate;
+    private String repaymentType;
+    private String product;
+    private String loanAmount;
+
+    public int getLoanId() {
+        return loanId;
+    }
+
+    public void setLoanId(int loanId) {
+        this.loanId = loanId;
+    }
+
+    public String getLoanNum() {
+        return loanNum;
+    }
+
+    public void setLoanNum(String loanNum) {
+        this.loanNum = loanNum;
+    }
+
+    public String getCustNum() {
+        return custNum;
+    }
+
+    public void setCustNum(String custNum) {
+        this.custNum = custNum;
+    }
+
+    public String getCustName() {
+        return custName;
+    }
+
+    public void setCustName(String custName) {
+        this.custName = custName;
+    }
+
+    public String getLoanDate() {
+        return loanDate;
+    }
+
+    public void setLoanDate(String loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public String getRepaymentType() {
+        return repaymentType;
+    }
+
+    public void setRepaymentType(String repaymentType) {
+        this.repaymentType = repaymentType;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public String getLoanAmount() {
+        return loanAmount;
+    }
+
+    public void setLoanAmount(String loanAmount) {
+        this.loanAmount = loanAmount;
+    }
+}
